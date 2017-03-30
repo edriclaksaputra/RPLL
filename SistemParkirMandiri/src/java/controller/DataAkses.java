8d1e8a68b776b824fb139002be91982eae6aa36f
chemyfio <fio.nat.valen@gmail.com>
2017-03-31 00:19:22 +0700

    bikin data akses

--- SistemParkirMandiri/src/java/controller/DataAkses.java
+++ SistemParkirMandiri/src/java/controller/DataAkses.java
@@ -5,10 +5,24 @@
  */
 package controller;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  *
  * @author Lenovo
  */
 public class DataAkses {
+    private static SessionFactory factory;
+    
+    public DataAkses(){
+        try{
+            factory = new Configuration().configure().buildSessionFactory();
+        }catch(Throwable ex){
+            System.err.println("Failed to create sessionFactory object."+ ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
     
 }
